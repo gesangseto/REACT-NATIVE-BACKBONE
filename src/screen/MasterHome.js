cf8d8ea511f5b14829f19ac7703733330dada7fc
unknown <gesang@merindo.co.id>
2021-12-03 13:57:01 +0700

    Add icon color on bottom

--- src/screen/MasterHome.js
+++ src/screen/MasterHome.js
@@ -4,14 +4,63 @@ import React from 'react';
 import Home from './tab_screen/Home';
 import Event from './tab_screen/Event';
 import Setting from './tab_screen/Setting';
+import IconMat from 'react-native-vector-icons/MaterialIcons';
 const Tab = createBottomTabNavigator();
 
 const MasterHome = ({route, navigation}) => {
   return (
     <Tab.Navigator screenOptions={{headerShown: false}}>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Event" component={Event} />
-      <Tab.Screen name="Setting" component={Setting} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <IconMat
+                name="home"
+                size={focused ? 35 : 30}
+                color={focused ? 'blue' : 'grey'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Event"
+        component={Event}
+        options={{
+          title: 'Event',
+          tabBarIcon: ({size, focused, color}) => {
+            if (focused) {
+              console.log('Focuse');
+            }
+            return (
+              <IconMat
+                name="calendar-today"
+                size={focused ? 35 : 30}
+                color={focused ? 'blue' : 'grey'}
+              />
+            );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Setting}
+        options={{
+          title: 'Setting',
+          tabBarIcon: ({size, focused, color}) => {
+            return (
+              <IconMat
+                name="app-settings-alt"
+                size={focused ? 35 : 30}
+                color={focused ? 'blue' : 'grey'}
+              />
+            );
+          },
+        }}
+      />
     </Tab.Navigator>
   );
 };
